8c2b98be5a3041cc69a5f27017ec5aa2c64dd7c3
Fares Tri <108296391+210411100141@users.noreply.github.com>
2023-06-06 00:24:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,12 +10,9 @@ from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
 import plotly.figure_factory as ff
-from sklearn.metrics import accuracy_score
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
-import seaborn as sns
-
-
+from sklearn.metrics import silhouette_score
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
 
 df = pd.read_csv("diabetes.csv")
 
@@ -28,131 +25,83 @@ st.write(df.describe())
 tab1, tab2 = st.tabs(["Deskripsi Data", "Tab Visualisasi data"])
 
 with tab1:
-   st.image("ar.png")
+    st.image("ar.png")
 
 with tab2:
-   # X AND Y DATA
-   x = df.drop(['Outcome'], axis = 1)
-   y = df.iloc[:, -1]
-   x_train, x_test, y_train, y_test = train_test_split(x,y, test_size = 0.2, random_state = 0)
-   
-   # FUNCTION
-   def user_report():
-      pregnancies = st.sidebar.slider('Pregnancies', 0,17, 3 )
-      glucose = st.sidebar.slider('Glucose', 0,200, 120 )
-      bp = st.sidebar.slider('Blood Pressure', 0,122, 70 )
-      skinthickness = st.sidebar.slider('Skin Thickness', 0,100, 20 )
-      insulin = st.sidebar.slider('Insulin', 0,846, 79 )
-      bmi = st.sidebar.slider('BMI', 0,67, 20 )
-      dpf = st.sidebar.slider('Diabetes Pedigree Function', 0.0,2.4, 0.47 )
-      age = st.sidebar.slider('Age', 21,88, 33 )
-      
-      user_report_data = {
-         'pregnancies':pregnancies,
-         'glucose':glucose,
-         'bp':bp,
-         'skinthickness':skinthickness,
-         'insulin':insulin,
-         'bmi':bmi,
-         'dpf':dpf,
-         'age':age
-      }
-      report_data = pd.DataFrame(user_report_data, index=[0])
-      return report_data
-     
-   # PATIENT DATA
-   user_data = user_report()
-   st.subheader('Patient Data')
-   st.write(user_data)
+    # X AND Y DATA
+    x = df.drop(['Outcome'], axis=1)
+    scaler = StandardScaler()
+    x_train = scaler.fit_transform(x)
     
-   # MODEL
-   rf  = RandomForestClassifier()
-   rf.fit(x_train, y_train)
-   user_result = rf.predict(user_data)
-      
-   # COLOR FUNCTION
-   if user_result[0]==0:
-      color = 'blue'
-   else:
-      color = 'red'
-   
-   # Age vs Pregnancies
-   st.header('Pregnancy count Graph (Others vs Yours)')
-   fig_preg = plt.figure()
-   ax1 = sns.scatterplot(x = 'Age', y = 'Pregnancies', data = df, hue = 'Outcome', palette = 'Greens')
-   ax2 = sns.scatterplot(x = user_data['age'], y = user_data['pregnancies'], s = 150, color = color)
-   plt.xticks(np.arange(10,100,5))
-   plt.yticks(np.arange(0,20,2))
-   plt.title('0 - Healthy & 1 - Unhealthy')
-   st.pyplot(fig_preg)
-   
-   # Age vs Glucose
-   st.header('Glucose Value Graph (Others vs Yours)')
-   fig_glucose = plt.figure()
-   ax3 = sns.scatterplot(x = 'Age', y = 'Glucose', data = df, hue = 'Outcome' , palette='magma')
-   ax4 = sns.scatterplot(x = user_data['age'], y = user_data['glucose'], s = 150, color = color)
-   plt.xticks(np.arange(10,100,5))
-   plt.yticks(np.arange(0,220,10))
-   plt.title('0 - Healthy & 1 - Unhealthy')
-   st.pyplot(fig_glucose)
+    # FUNCTION
+    def user_report():
+        pregnancies = st.sidebar.slider('Pregnancies', 0, 17, 3)
+        glucose = st.sidebar.slider('Glucose', 0, 200, 120)
+        bp = st.sidebar.slider('Blood Pressure', 0, 122, 70)
+        skinthickness = st.sidebar.slider('Skin Thickness', 0, 100, 20)
+        insulin = st.sidebar.slider('Insulin', 0, 846, 79)
+        bmi = st.sidebar.slider('BMI', 0, 67, 20)
+        dpf = st.sidebar.slider('Diabetes Pedigree Function', 0.0, 2.4, 0.47)
+        age = st.sidebar.slider('Age', 21, 88, 33)
 
-   # Age vs Bp
-   st.header('Blood Pressure Value Graph (Others vs Yours)')
-   fig_bp = plt.figure()
-   ax5 = sns.scatterplot(x = 'Age', y = 'BloodPressure', data = df, hue = 'Outcome', palette='Reds')
-   ax6 = sns.scatterplot(x = user_data['age'], y = user_data['bp'], s = 150, color = color)
-   plt.xticks(np.arange(10,100,5))
-   plt.yticks(np.arange(0,130,10))
-   plt.title('0 - Healthy & 1 - Unhealthy')
-   st.pyplot(fig_bp)
+        user_report_data = {
+            'pregnancies': pregnancies,
+            'glucose': glucose,
+            'bp': bp,
+            'skinthickness': skinthickness,
+            'insulin': insulin,
+            'bmi': bmi,
+            'dpf': dpf,
+            'age': age
+        }
+        report_data = pd.DataFrame(user_report_data, index=[0])
+        report_data_scaled = scaler.transform(report_data)
+        return report_data_scaled
 
-   # Age vs St
-   st.header('Skin Thickness Value Graph (Others vs Yours)')
-   fig_st = plt.figure()
-   ax7 = sns.scatterplot(x = 'Age', y = 'SkinThickness', data = df, hue = 'Outcome', palette='Blues')
-   ax8 = sns.scatterplot(x = user_data['age'], y = user_data['skinthickness'], s = 150, color = color)
-   plt.xticks(np.arange(10,100,5))
-   plt.yticks(np.arange(0,110,10))
-   plt.title('0 - Healthy & 1 - Unhealthy')
-   st.pyplot(fig_st)
+    # PATIENT DATA
+    user_data = user_report()
+    st.subheader('Patient Data')
+    st.write(user_data)
 
-   # Age vs Insulin
-   st.header('Insulin Value Graph (Others vs Yours)')
-   fig_i = plt.figure()
-   ax9 = sns.scatterplot(x = 'Age', y = 'Insulin', data = df, hue = 'Outcome', palette='rocket')
-   ax10 = sns.scatterplot(x = user_data['age'], y = user_data['insulin'], s = 150, color = color)
-   plt.xticks(np.arange(10,100,5))
-   plt.yticks(np.arange(0,900,50))
-   plt.title('0 - Healthy & 1 - Unhealthy')
-   st.pyplot(fig_i)
+    # MODEL
+    k = 2  # Number of clusters
+    kmeans = KMeans(n_clusters=k, random_state=0)
+    kmeans.fit(x_train)
+    user_result = kmeans.predict(user_data)
 
-   # Age vs BMI
-   st.header('BMI Value Graph (Others vs Yours)')
-   fig_bmi = plt.figure()
-   ax11 = sns.scatterplot(x = 'Age', y = 'BMI', data = df, hue = 'Outcome', palette='rainbow')
-   ax12 = sns.scatterplot(x = user_data['age'], y = user_data['bmi'], s = 150, color = color)
-   plt.xticks(np.arange(10,100,5))
-   plt.yticks(np.arange(0,70,5))
-   plt.title('0 - Healthy & 1 - Unhealthy')
-   st.pyplot(fig_bmi)
+    # COLOR FUNCTION
+    if user_result[0] == 0:
+        color = 'blue'
+    else:
+        color = 'red'
 
-   # Age vs Dpf
-   st.header('DPF Value Graph (Others vs Yours)')
-   fig_dpf = plt.figure()
-   ax13 = sns.scatterplot(x = 'Age', y = 'DiabetesPedigreeFunction', data = df, hue = 'Outcome', palette='YlOrBr')
-   ax14 = sns.scatterplot(x = user_data['age'], y = user_data['dpf'], s = 150, color = color)
-   plt.xticks(np.arange(10,100,5))
-   plt.yticks(np.arange(0,3,0.2))
-   plt.title('0 - Healthy & 1 - Unhealthy')
-   st.pyplot(fig_dpf)
+    # Age vs Glucose
+    st.header('Glucose Value Graph (Others vs Yours)')
+    fig_glucose = plt.figure()
+    ax3 = sns.scatterplot(x='Age', y='Glucose', data=df, hue='Outcome', palette='magma')
+    ax4 = sns.scatterplot(x=user_data[:, 7], y=user_data[:, 1], s=150, color=color)
+    plt.xticks(np.arange(10, 100, 5))
+    plt.yticks(np.arange(0, 220, 10))
+    plt.title('0 - Healthy & 1 - Unhealthy')
+    st.pyplot(fig_glucose)
+
+    # Age vs Bp
+    st.header('Blood Pressure Value Graph (Others vs Yours)')
+    fig_bp = plt.figure()
+    ax5 = sns.scatterplot(x='Age', y='BloodPressure', data=df, hue='Outcome', palette='Reds')
+    ax6 = sns.scatterplot(x=user_data[:, 7], y=user_data[:, 2], s=150, color=color)
+    plt.xticks(np.arange(10, 100, 5))
+    plt.yticks(np.arange(0, 130, 10))
+    plt.title('0 - Healthy & 1 - Unhealthy')
+    st.pyplot(fig_bp)
 
 # OUTPUT
 st.subheader('Your Report: ')
-output=''
-if user_result[0]==0:
-  output = 'You are not Diabetic'
+output = ''
+if user_result[0] == 0:
+    output = 'You are not Diabetic'
 else:
-  output = 'You are Diabetic'
+    output = 'You are Diabetic'
 st.title(output)
-st.subheader('Accuracy: ')
-st.write(str(accuracy_score(y_test, rf.predict(x_test))*100)+'%')
+st.subheader('Silhouette Score: ')
+st.write(str(silhouette_score(x_train, kmeans.labels_)) + '%')

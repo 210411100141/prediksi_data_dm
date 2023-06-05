0808fb4495bb5ca61a73895d37a78664d0bfb075
Fares Tri <108296391+210411100141@users.noreply.github.com>
2023-06-05 23:48:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,49 +2,47 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from sklearn.cluster import KMeans
-from sklearn.metrics import accuracy_score
 
-# Mengambil data diabetes dari file CSV atau DataFrame Anda
-data_path = "dm.csv"
-df = pd.read_csv(data_path)
-
-# Menghilangkan kolom Outcome karena itu adalah label yang akan diprediksi
-X = df.drop("Outcome", axis=1)
-
-# Membuat fungsi untuk melakukan clustering dengan metode K-means
 def kmeans_clustering(data, num_clusters):
     kmeans = KMeans(n_clusters=num_clusters, random_state=0)
     kmeans.fit(data)
     labels = kmeans.labels_
-    return labels
+    centroids = kmeans.cluster_centers_
+    return labels, centroids
 
-# Membuat halaman dengan Streamlit
 def main():
-    st.title("Aplikasi Clustering K-means untuk Data Penyakit Diabetes")
-    
-    # Menampilkan data awal
-    st.subheader("Data Penyakit Diabetes")
-    st.dataframe(df)
-    
-    # Mengambil jumlah cluster dari pengguna
-    num_clusters = st.slider("Jumlah Cluster", min_value=2, max_value=10, value=3)
-    
-    # Melakukan clustering dengan metode K-means
-    labels = kmeans_clustering(X, num_clusters)
-    
-    # Menampilkan hasil clustering
-    st.subheader("Hasil Clustering")
-    df_result = df.copy()
-    df_result["Cluster"] = labels
-    st.dataframe(df_result)
-    
-    # Menghitung akurasi clustering dengan menggunakan kolom "Outcome" sebagai label
-    true_labels = df["Outcome"]
-    accuracy = accuracy_score(true_labels, labels)
-    
-    # Menampilkan akurasi clustering
-    st.subheader("Akurasi Clustering")
-    st.write(f"Akurasi: {accuracy}")
-    
+    st.title("Aplikasi K-means Clustering")
+
+    # Upload dataset
+    st.subheader("Upload Dataset")
+    file = st.file_uploader("Upload file CSV", type=["csv"])
+    if file is not None:
+        data = pd.read_csv(dm.scv)
+
+        # Menampilkan data
+        st.subheader("Data")
+        st.write(data)
+
+        # Memilih atribut yang digunakan untuk clustering
+        selected_features = st.multiselect("Pilih fitur untuk clustering", data.columns)
+
+        # Memilih jumlah cluster
+        num_clusters = st.slider("Pilih jumlah cluster", min_value=2, max_value=10)
+
+        if st.button("Clustering"):
+            selected_data = data[selected_features]
+            labels, centroids = kmeans_clustering(selected_data, num_clusters)
+
+            # Menambahkan kolom hasil clustering pada dataset
+            data["Cluster"] = labels
+
+            # Menampilkan hasil clustering
+            st.subheader("Hasil Clustering")
+            st.write(data)
+
+            # Menampilkan nilai centroid untuk setiap cluster
+            st.subheader("Nilai Centroid")
+            st.write(pd.DataFrame(centroids, columns=selected_features))
+
 if __name__ == "__main__":
     main()

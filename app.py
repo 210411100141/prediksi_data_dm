ee9318653bb796db34fad77fb17983a82d2da152
Fares Tri <108296391+210411100141@users.noreply.github.com>
2023-06-10 00:03:19 +0700

    Update app.py

--- app.py
+++ app.py
@@ -64,33 +64,26 @@ with tab1:
        plot_diabetes_vs_normal(attribute)
 
 with tab2:
-   # X AND Y DATA
-   x = df.drop(['Outcome'], axis=1)
-   y = df.iloc[:, -1]
-   scaler = MinMaxScaler()
-   x_scaled = scaler.fit_transform(x)
-   
-   # FUNCTION
-   def user_report():
-      usia = st.sidebar.slider('Usia', 15, 80, 20)
-      glukosa = st.sidebar.slider('Glukosa', 70, 200, 90)
-      td = st.sidebar.slider('Tekanan darah', 0, 110, 70)
-      kk = st.sidebar.slider('Ketebalan Kulit', 0, 50, 25)
-      insulin = st.sidebar.slider('Insulin', 0, 900, 200)
-      bmi = st.sidebar.slider('BMI', 20, 450, 50)
-      
-      
-      user_report_data = {
-         'usia': usia,
-         'glukosa': glukosa,
-         'td': td,
-         'kk': kk,
-         'insulin': insulin,
-         'bmi': bmi,
-      }
-      report_data = pd.DataFrame(user_report_data, index=[0])
-      return report_data
-     
-  
-# OUTPUT
+   # Fungsi untuk menghitung skor siluet dari clustering K-means
+   def calculate_silhouette_score(attribute):
+       data = df[[attribute]].values
+       scaler = MinMaxScaler()
+       data_scaled = scaler.fit_transform(data)
+       kmeans = KMeans(n_clusters=2, random_state=0)
+       kmeans.fit(data_scaled)
+       labels = kmeans.labels_
+       silhouette = silhouette_score(data_scaled, labels)
+       return silhouette
 
+   # Daftar atribut untuk clustering
+   attributes = df.columns[:-1]  # Mengambil semua kolom kecuali kolom Outcome
+
+   # Melakukan perhitungan skor siluet pada setiap atribut
+   silhouette_scores = {}
+   for attribute in attributes:
+       silhouette_scores[attribute] = calculate_silhouette_score(attribute)
+
+# Menampilkan skor siluet untuk setiap atribut
+st.subheader('Silhouette Scores:')
+for attribute, score in silhouette_scores.items():
+    st.write(f'{attribute}: {score}')

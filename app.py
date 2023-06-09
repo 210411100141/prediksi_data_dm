7a90517c823044a51d19a5f40b6119ff8ddde6d0
Fares Tri <108296391+210411100141@users.noreply.github.com>
2023-06-10 00:14:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -24,42 +24,6 @@ st.write(df.describe())
 
 tab1, tab2, = st.tabs(["Clustering","Tab Visualisasi data"])
 
-# Fungsi untuk menghitung skor siluet dari clustering K-means Seluruh data
-def calculate_silhouette_score(attribute):
-    data = df[[attribute]].values
-    scaler = MinMaxScaler()
-    data_scaled = scaler.fit_transform(data)
-    kmeans = KMeans(n_clusters=2, random_state=0)
-    kmeans.fit(data_scaled)
-    labels = kmeans.labels_
-    silhouette = silhouette_score(data_scaled, labels)
-    return silhouette
-
-# Daftar atribut untuk clustering
-attributes = df.columns[:-1]  # Mengambil semua kolom kecuali kolom Outcome
-
-# Melakukan perhitungan skor siluet pada setiap atribut
-silhouette_scores = {}
-for attribute in attributes:
-    silhouette_scores[attribute] = calculate_silhouette_score(attribute)
-
-# Menampilkan skor siluet untuk setiap atribut
-st.subheader('Silhouette Scores:')
-for attribute, score in silhouette_scores.items():
-    st.write(f'{attribute}: {score}')
-
-# Menggabungkan semua atribut menjadi satu dataset
-all_data = df.drop('Outcome', axis=1)
-all_scaled = scaler.fit_transform(all_data)
-kmeans_all = KMeans(n_clusters=2, random_state=0)
-kmeans_all.fit(all_scaled)
-labels_all = kmeans_all.labels_
-silhouette_all = silhouette_score(all_scaled, labels_all)
-
-# Menampilkan skor siluet untuk seluruh data
-st.subheader('Silhouette Score for All Data:')
-st.write(silhouette_all)
-
 with tab1:
    # Fungsi untuk menampilkan grafik perbandingan diabetes vs normal pada suatu atribut
    def plot_diabetes_vs_normal(attribute):
